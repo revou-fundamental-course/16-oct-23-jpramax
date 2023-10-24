a93b6ad1e45d799a898b94093720679a2289a6f2
jpramax <joelian.pramana@gmail.com>
2023-10-24 12:13:10 +0700

    Menambahkan javascript Contact Us

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function validateForm(){
+    const name      = document.forms['message-form']['full-name'].value
+    const birthDate = document.forms['message-form']['birth-date'].value;
+    const gender    = document.forms['message-form']['gender'].value;
+    const messages  = document.forms['message-form']['messages'].value;
+
+    if( name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Isian Data tidak boleh ada yang kosong");
+        return false;
+    }
+
+    setSenderUI(name,birthData,gender, messages);
+
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages){
+    document.getElementById("sender-full-name").innerHTML=name;
+    document.getElementById('sender-birth-date').innerHTML=birthDate;
+    document.getElementById('sender-gender').innerHTML=gender;
+    document.getElementById('sender-messages').innerHTML=messages;
+}
